136750c4990452ef3b11586338cfd8b5c2ade852
Ryan <ryansiew2001@gmail.com>
2024-03-19 12:08:24 +0800

    Ryan Update 1

--- DIP215_ASG/src/Course.java
+++ DIP215_ASG/src/Course.java
@@ -1,2 +1,40 @@
+package DIP215_ASG.src;
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private int creditHours;
+
+    // Constructor
+    public Course(String courseId, String courseName, int creditHours) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.creditHours = creditHours;
+    }
+
+    // Getter methods
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    // Setter methods
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
 }
--- DIP215_ASG/src/Student.java
+++ DIP215_ASG/src/Student.java
@@ -1,2 +1,43 @@
+package DIP215_ASG.src;
+import java.util.ArrayList;
+
 public class Student {
+    private String studentID;
+    private String studentName;
+    private String studentType;
+    private int totalCreditHours;
+    private ArrayList<Course> coursesEnrolled;
+
+    public Student(String studentID, String studentName, String studentType, int totalCreditHours){
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.studentType = studentType;
+        this.totalCreditHours = totalCreditHours;
+    }
+
+    public String getStudentId(){
+        return studentID;
+    }
+
+    public String getStudentName(){
+        return studentName;
+    }
+
+    public String getStudentType(){
+        return studentType;
+    }
+
+    public int getCreditHours() {
+        return totalCreditHours;
+    }
+
+    // Method to add a course to the enrolled courses list
+    public void enrollCourse(Course course) {
+        coursesEnrolled.add(course);
+    }
+
+    // Method to get the list of enrolled courses
+    public ArrayList<Course> getCoursesEnrolled() {
+        return coursesEnrolled;
+    }
 }
