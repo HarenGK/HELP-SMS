345c812623f9c5b33676d4c3cc91e6cb9fbd38cd
WCYG22 <wongchengyong100@gmail.com>
2024-04-03 11:02:41 +0800

    change teacher java

--- DIP215_ASG/src/Teacher.java
+++ DIP215_ASG/src/Teacher.java
@@ -1,4 +1,5 @@
 package DIP215_ASG.src;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,38 +8,28 @@ public class Teacher {
     private String teacherName;
     private List<Course> coursesTaught;
     private String teacherQualifications;
-    private List<Course> teacherAssignedCourses;
 
     public Teacher(String teacherId, String teacherName, String teacherQualifications) {
         this.teacherId = teacherId;
         this.teacherName = teacherName;
         this.teacherQualifications = teacherQualifications;
         this.coursesTaught = new ArrayList<>();
-        this.teacherAssignedCourses = new ArrayList<>();
     }
 
-    public void getGrade() {
-        // Placeholder for the method implementation
-    }
-
-    public List<Course> getCourses() {
-        return coursesTaught;
-    }
-
-    public void setCourse(Course course) {
+    public void addCourse(Course course) {
         this.coursesTaught.add(course);
     }
 
-    public void getTeacherInfo() {
-        // Printing teacher's information to the console
+    public void printTeacherInfo() {
         System.out.println("Teacher ID: " + teacherId);
         System.out.println("Teacher Name: " + teacherName);
         System.out.println("Qualifications: " + teacherQualifications);
         System.out.println("Courses Assigned:");
-        for (Course course : teacherAssignedCourses) {
-            System.out.println("- " );
+        for (Course course : coursesTaught) {
+            System.out.println("- " + course.getCourseName());
         }
     }
+
     public String getTeacherQualifications() {
         return teacherQualifications;
     }
@@ -46,8 +37,6 @@ public class Teacher {
     public void setTeacherQualifications(String qualifications) {
         this.teacherQualifications = qualifications;
     }
-
-    // Getters and setters for all other fields
 
     public String getTeacherId() {
         return teacherId;
@@ -65,13 +54,7 @@ public class Teacher {
         this.teacherName = teacherName;
     }
 
-    public List<Course> getTeacherAssignedCourses() {
-        return teacherAssignedCourses;
+    public List<Course> getCoursesTaught() {
+        return coursesTaught;
     }
-
-    public void setTeacherAssignedCourses(List<Course> teacherAssignedCourses) {
-        this.teacherAssignedCourses = teacherAssignedCourses;
-    }
-
-    // Additional methods to manipulate coursesTaught and teacherAssignedCourses
 }
